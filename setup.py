5350128495b2594dd189346faf7889eddf217567
pc532627 <138115355+pc532627@users.noreply.github.com>
2024-03-13 09:58:28 +0000

    feat: cap lower version of numpy to ensure typign exists.
    Refs: #94

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
     packages=find_packages(include=["vanguard", "vanguard.*"]),
-    install_requires=["bibtexparser", "gpytorch>=1.5.1", "matplotlib", "numpy", "pandas",
+    install_requires=["bibtexparser", "gpytorch>=1.5.1", "matplotlib", "numpy>=1.20", "pandas",
                       "scikit-learn", "scikit-learn-extra", "scipy", "torch>=1.7.0", "urllib3"],
     package_data={"vanguard": [BIBTEX_FILE]}
 )
