875476c843ceeebf3995264632f2971e52e8a523
rg936672 <162452529+rg936672@users.noreply.github.com>
2024-04-09 09:42:38 +0100

    fix: bump urllib3 to 2.2.1 in setup.py as well to fix import error

--- setup.py
+++ setup.py
@@ -28,8 +28,18 @@ setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
     packages=find_packages(include=["vanguard", "vanguard.*"]),
-    install_requires=["bibtexparser", "gpytorch>=1.5.1", "matplotlib", "numpy", "pandas",
-                      "scikit-learn", "scikit-learn-extra", "scipy", "torch>=1.7.0", "urllib3"],
+    install_requires=[
+        "bibtexparser",
+        "gpytorch>=1.5.1",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "scikit-learn-extra",
+        "scipy",
+        "torch>=1.7.0",
+        "urllib3>=2.2.1"
+    ],
     package_data={"vanguard": [BIBTEX_FILE]}
 )
 
