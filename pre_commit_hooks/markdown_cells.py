0d5fbb66e8435ffeaf88bd6ce21fa3aaa31079c5
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2024-04-24 12:37:52 +0100

    Removed markdown cells from Jupyter notebook cells, and added custom pre-commit hook to check for them

---
+++ pre_commit_hooks/markdown_cells.py
@@ -0,0 +1,40 @@
+"""
+A simple pre-commit hook which forbids markdown cells in Jupyter notebooks.
+"""
+import argparse
+from typing import Optional, Sequence
+
+import nbformat
+
+
+def count_markdown_cells_in_notebook(file_path: str) -> int:
+    """Check a file and return the number of lambdas present."""
+    with open(file_path) as rf:
+        notebook = nbformat.read(rf, as_version=4)
+
+    number_of_bad_cells = 0
+
+    for cell_number, cell in enumerate(notebook.cells, start=1):
+        if cell["cell_type"] == "markdown":
+            number_of_bad_cells += 1
+            source_preview = str(cell.source).replace("\n", " ")[:20]
+            print(f"{file_path}:cell_{cell_number}: Markdown cell: '{source_preview}...'")
+
+    return number_of_bad_cells
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="Filenames to fix")
+    args = parser.parse_args(argv)
+
+    number_of_bad_cells = 0
+
+    for filename in args.filenames:
+        number_of_bad_cells += count_markdown_cells_in_notebook(filename)
+
+    return int(bool(number_of_bad_cells))
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
