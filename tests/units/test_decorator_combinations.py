e847d3d53c61e052fc48571c04c1675916eb71bd
rg936672 <162452529+rg936672@users.noreply.github.com>
2024-07-09 12:39:48 +0100

    test: refactor test_decorator_combinations.py to use pytest parametrize

--- tests/units/test_decorator_combinations.py
+++ tests/units/test_decorator_combinations.py
@@ -4,11 +4,11 @@ Tests for the pairwise combinations of decorators.
 
 import itertools
 import re
-import unittest
 from collections.abc import Generator
 from typing import Any, Callable, Dict, Optional, Tuple, TypeVar
 from unittest.mock import patch
 
+import pytest
 from gpytorch.kernels import RBFKernel
 from gpytorch.likelihoods import BernoulliLikelihood, DirichletClassificationLikelihood, FixedNoiseGaussianLikelihood
 from gpytorch.mlls import VariationalELBO
@@ -122,132 +122,139 @@ EXPECTED_COMBINATION_FIT_ERRORS = {
 }
 
 
-class CombinationTests(unittest.TestCase):
-    """
-    Tests for the pairwise combinations of decorators.
-    """
+def _yield_initialised_decorators() -> "Generator[Tuple[Callable, Callable, dict[str, Any], Dataset], None, None]":
+    """Yield pairs of initialised decorators."""
+    for upper_decorator_details, lower_decorator_details in itertools.permutations(DECORATORS.items(), r=2):
+        upper_decorator, upper_controller_kwargs, upper_dataset = _create_decorator(upper_decorator_details)
+        lower_decorator, lower_controller_kwargs, lower_dataset = _create_decorator(lower_decorator_details)
 
-    def test_combinations(self) -> None:
-        # pylint: disable=broad-exception-caught
-        # If/when these tests are upgraded to use pytest, we can just let the exceptions be raised, rather than
-        # explicitly transforming them into test failures.
-        """Shouldn't throw any errors."""
-        for upper_decorator, lower_decorator, controller_kwargs, dataset in self._yield_initialised_decorators():
-            with self.subTest(upper=type(upper_decorator).__name__, lower=type(lower_decorator).__name__):
-                try:
-                    controller_class = upper_decorator(lower_decorator(GaussianGPController))
-                except (MissingRequirementsError, TopmostDecoratorError):
-                    continue
+        combination = (type(upper_decorator), type(lower_decorator))
+        reversed_combination = (type(lower_decorator), type(upper_decorator))
+        if combination in EXCLUDED_COMBINATIONS or reversed_combination in EXCLUDED_COMBINATIONS:
+            continue
 
-                final_kwargs = {
-                    "train_x": dataset.train_x,
-                    "train_y": dataset.train_y,
-                    "y_std": dataset.train_y_std,
-                    "kernel_class": ScaledRBFKernel,
-                    "rng": get_default_rng(),
-                }
+        if upper_dataset and lower_dataset:
+            raise RuntimeError(
+                f"Cannot combine {type(upper_decorator).__name__} and "
+                f"{type(lower_decorator).__name__}: two datasets have been passed."
+            )
 
-                combination = (type(upper_decorator), type(lower_decorator))
-                combination_controller_kwargs = COMBINATION_CONTROLLER_KWARGS.get(combination, {})
-                final_kwargs.update(controller_kwargs)
-                final_kwargs.update(combination_controller_kwargs)
-
-                try:
-                    controller = controller_class(**final_kwargs)
-                except Exception as error:
-                    self.fail(f"Could not initialise: {error}")
-
-                try:
-                    controller.fit(1)
-                except Exception as error:
-                    try:
-                        expected_error_class, expected_error_message = EXPECTED_COMBINATION_FIT_ERRORS[combination]
-                    except KeyError:
-                        self.fail(f"Could not train: {error}")
-
-                    if type(error) != expected_error_class:  # pylint: disable=unidiomatic-typecheck
-                        # using type() here rather than isinstance() as we do specifically want the given error class
-                        # and not some subclass
-                        self.fail(f"Expected {expected_error_class} but got: {error}")
-                    elif not re.match(expected_error_message, str(error)):
-                        self.fail(f"Expected error with the message {expected_error_message!r} but got: {error}")
-                    else:
-                        return
-
-                try:
-                    posterior = controller.posterior_over_point(dataset.test_x)
-                except Exception as error:
-                    if not hasattr(controller, "classify_points"):
-                        self.fail(f"Could not get posterior: {error}")
-                else:
-                    try:
-                        posterior.prediction()
-                    except Exception as error:
-                        if isinstance(upper_decorator, SetWarp) or isinstance(lower_decorator, SetWarp):
-                            pass
-                        else:
-                            self.fail(f"Could not predict: {error}")
-
-                    try:
-                        posterior.confidence_interval(dataset.significance)
-                    except Exception as error:
-                        self.fail(f"Could not predict: {error}")
-
-                try:
-                    # we don't care about any kind of accuracy here, so just pick the minimum number that doesn't
-                    # cause errors
-                    with patch.object(MonteCarloPosteriorCollection, "INITIAL_NUMBER_OF_SAMPLES", 4):
-                        fuzzy_posterior = controller.posterior_over_fuzzy_point(dataset.test_x, dataset.test_x_std)
-                except Exception as error:
-                    if not hasattr(controller, "classify_points"):
-                        self.fail(f"Could not get posterior: {error}")
-                else:
-                    try:
-                        fuzzy_posterior.prediction()
-                    except Exception as error:
-                        if isinstance(upper_decorator, SetWarp) or isinstance(lower_decorator, SetWarp):
-                            pass
-                        else:
-                            self.fail(f"Could not predict: {error}")
-
-                    try:
-                        # again, we don't care about accuracy, so just pick the minimum number that doesn't cause errors
-                        with patch.object(MonteCarloPosteriorCollection, "_decide_mc_num_samples", lambda *_: 4):
-                            fuzzy_posterior.confidence_interval(dataset.significance)
-                    except Exception as error:
-                        self.fail(f"Could not predict: {error}")
-
-    def _yield_initialised_decorators(
-        self,
-    ) -> "Generator[Tuple[Callable, Callable, dict[str, Any], Dataset], None, None]":
-        """Yield pairs of initialised decorators."""
-        for upper_decorator_details, lower_decorator_details in itertools.permutations(DECORATORS.items(), r=2):
-            upper_decorator, upper_controller_kwargs, upper_dataset = self._create_decorator(upper_decorator_details)
-            lower_decorator, lower_controller_kwargs, lower_dataset = self._create_decorator(lower_decorator_details)
-
-            combination = (type(upper_decorator), type(lower_decorator))
-            reversed_combination = (type(lower_decorator), type(upper_decorator))
-            if combination in EXCLUDED_COMBINATIONS or reversed_combination in EXCLUDED_COMBINATIONS:
-                continue
-
-            if upper_dataset and lower_dataset:
-                raise RuntimeError(
-                    f"Cannot combine {type(upper_decorator).__name__} and "
-                    f"{type(lower_decorator).__name__}: two datasets have been passed."
-                )
-
-            dataset = (
+        dataset = (
                 upper_dataset
                 or lower_dataset
                 or SyntheticDataset(n_train_points=20, n_test_points=2, rng=get_default_rng())
-            )
+        )
 
-            controller_kwargs = {**upper_controller_kwargs, **lower_controller_kwargs}
-            yield upper_decorator, lower_decorator, controller_kwargs, dataset
+        controller_kwargs = {**upper_controller_kwargs, **lower_controller_kwargs}
+        yield upper_decorator, lower_decorator, controller_kwargs, dataset
 
-    @staticmethod
-    def _create_decorator(details: Tuple[Callable, Dict[str, Any]]) -> Tuple[Callable, ControllerT, Optional[Dataset]]:
-        """Unpack decorator details."""
-        decorator_class, all_decorator_kwargs = details
-        decorator = decorator_class(ignore_all=True, **all_decorator_kwargs["decorator"])
-        return decorator, all_decorator_kwargs["controller"], all_decorator_kwargs.get("dataset", None)
+
+def _create_decorator(details: Tuple[Callable, Dict[str, Any]]) -> Tuple[Callable, ControllerT, Optional[Dataset]]:
+    """Unpack decorator details."""
+    decorator_class, all_decorator_kwargs = details
+    decorator = decorator_class(ignore_all=True, **all_decorator_kwargs["decorator"])
+    return decorator, all_decorator_kwargs["controller"], all_decorator_kwargs.get("dataset", None)
+
+
+@pytest.mark.parametrize(
+    "upper_decorator, lower_decorator, controller_kwargs, dataset",
+    [
+        pytest.param(
+            upper_decorator,
+            lower_decorator,
+            controller_kwargs,
+            dataset,
+            id=f"Upper: {type(upper_decorator).__name__} - Lower: {type(lower_decorator).__name__}",
+        )
+        for upper_decorator, lower_decorator, controller_kwargs, dataset in _yield_initialised_decorators()
+    ],
+)
+def test_combinations(
+    upper_decorator: Callable, lower_decorator: Callable, controller_kwargs: dict[str, Any], dataset: Dataset
+) -> None:
+    # pylint: disable=broad-exception-caught
+    # If/when these tests are upgraded to use pytest, we can just let the exceptions be raised, rather than
+    # explicitly transforming them into test failures.
+    """Shouldn't throw any errors."""
+    try:
+        controller_class = upper_decorator(lower_decorator(GaussianGPController))
+    except (MissingRequirementsError, TopmostDecoratorError):
+        return
+
+    final_kwargs = {
+        "train_x": dataset.train_x,
+        "train_y": dataset.train_y,
+        "y_std": dataset.train_y_std,
+        "kernel_class": ScaledRBFKernel,
+    "rng": get_default_rng(),
+                }
+
+    combination = (type(upper_decorator), type(lower_decorator))
+    combination_controller_kwargs = COMBINATION_CONTROLLER_KWARGS.get(combination, {})
+    final_kwargs.update(controller_kwargs)
+    final_kwargs.update(combination_controller_kwargs)
+
+    try:
+        controller = controller_class(**final_kwargs)
+    except Exception as error:
+        pytest.fail(f"Could not initialise: {error}")
+
+    try:
+        controller.fit(1)
+    except Exception as error:
+        try:
+            expected_error_class, expected_error_message = EXPECTED_COMBINATION_FIT_ERRORS[combination]
+        except KeyError:
+            pytest.fail(f"Could not train: {error}")
+
+        if type(error) != expected_error_class:  # pylint: disable=unidiomatic-typecheck
+            # using type() here rather than isinstance() as we do specifically want the given error class
+            # and not some subclass
+            pytest.fail(f"Expected {expected_error_class} but got: {error}")
+        elif not re.match(expected_error_message, str(error)):
+            pytest.fail(f"Expected error with the message {expected_error_message!r} but got: {error}")
+        else:
+            return
+
+    try:
+        posterior = controller.posterior_over_point(dataset.test_x)
+    except Exception as error:
+        if not hasattr(controller, "classify_points"):
+            pytest.fail(f"Could not get posterior: {error}")
+    else:
+        try:
+            posterior.prediction()
+        except Exception as error:
+            if isinstance(upper_decorator, SetWarp) or isinstance(lower_decorator, SetWarp):
+                pass
+            else:
+                pytest.fail(f"Could not predict: {error}")
+
+        try:
+            posterior.confidence_interval(dataset.significance)
+        except Exception as error:
+            pytest.fail(f"Could not predict: {error}")
+
+    try:
+        # we don't care about any kind of accuracy here, so just pick the minimum number that doesn't
+        # cause errors
+        with patch.object(MonteCarloPosteriorCollection, "INITIAL_NUMBER_OF_SAMPLES", 4):
+            fuzzy_posterior = controller.posterior_over_fuzzy_point(dataset.test_x, dataset.test_x_std)
+    except Exception as error:
+        if not hasattr(controller, "classify_points"):
+            pytest.fail(f"Could not get posterior: {error}")
+    else:
+        try:
+            fuzzy_posterior.prediction()
+        except Exception as error:
+            if isinstance(upper_decorator, SetWarp) or isinstance(lower_decorator, SetWarp):
+                pass
+            else:
+                pytest.fail(f"Could not predict: {error}")
+
+        try:
+            # again, we don't care about accuracy, so just pick the minimum number that doesn't cause errors
+            with patch.object(MonteCarloPosteriorCollection, "_decide_mc_num_samples", lambda *_: 4):
+                fuzzy_posterior.confidence_interval(dataset.significance)
+        except Exception as error:
+            pytest.fail(f"Could not predict: {error}")
