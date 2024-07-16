38fa7b28ffbbe1d74a66c33bcc394d656524d05a
pc532627 <138115355+pc532627@users.noreply.github.com>
2024-07-16 10:13:11 +0100

    tests: quality pass on standardise code.
    Refs: 144

--- tests/units/test_standardise.py
+++ tests/units/test_standardise.py
@@ -17,7 +17,12 @@ from vanguard.vanilla import GaussianGPController
 
 
 class StandardiseModuleTests(unittest.TestCase):
+    """
+    Tests relating to standardisation of input data using the `StandardiseXModule` class.
+    """
+
     def setUp(self) -> None:
+        """Define data shared across tests."""
         self.data = torch.randn((30, 2)) * 3 + 4
         self.standardised_data = (self.data - self.data.mean(dim=0)) / self.data.std(dim=0)
         self.standardiser = StandardiseXModule.from_data(self.data, None, None)
@@ -34,7 +39,8 @@ class StandardiseModuleTests(unittest.TestCase):
         self.seed = torch.seed()
         self.rng = get_default_rng()
         self.base_mean = BaseMean()
-        torch.manual_seed(self.seed)  # reset the seed to reproduce the random parameters
+        # Reset the seed to reproduce the random parameters
+        torch.manual_seed(self.seed)
         self.scaled_mean = scaled_mean_class()
 
         self.base_kernel = RBFKernel()
@@ -43,6 +49,14 @@ class StandardiseModuleTests(unittest.TestCase):
     def _test_standardised_module_gives_same_results_as_plain_module_on_different_data(
         self, base_module: torch.nn.Module, scaled_module: torch.nn.Module
     ) -> None:
+        """
+        Test usage of scaled and unscaled modules on corresponding data.
+
+        Test that calling the scaled module on raw data agrees with calling the unscaled module on
+        scaled data. Here we expect the scaled module to internally scale the data before computations,
+        whereas the unscaled module should not apply any scaling, but since the data is already scaled,
+        the results should be the same.
+        """
         standard_output = self._to_numpy(scaled_module(self.data))
         plain_output = self._to_numpy(base_module(self.standardised_data))
         np.testing.assert_array_almost_equal(standard_output, plain_output)
@@ -50,6 +64,12 @@ class StandardiseModuleTests(unittest.TestCase):
     def _test_standardised_module_gives_different_results_to_plain_module_on_raw_data(
         self, base_module: torch.nn.Module, scaled_module: torch.nn.Module
     ) -> None:
+        """
+        Test that a scaled and unscaled module give different results on the same input data.
+
+        Here we pass a scaled module and an unscaled module the same (unscaled) data and expect
+        different results, since one should apply scaling before computations and one should not.
+        """
         standard_output = self._to_numpy(scaled_module(self.data))
         plain_output = self._to_numpy(base_module(self.data))
         np.testing.assert_array_less(-np.abs(standard_output - plain_output).mean(), -1e-6)
@@ -57,6 +77,12 @@ class StandardiseModuleTests(unittest.TestCase):
     def _test_standardised_module_gives_different_results_to_plain_module_on_standardised_data(
         self, base_module: torch.nn.Module, scaled_module: torch.nn.Module
     ) -> None:
+        """
+        Test that a scaled and unscaled module give different results on the same input data.
+
+        Here we pass a scaled module and an unscaled module the same (scaled) data and expect
+        different results, since one should apply scaling before computations and one should not.
+        """
         standard_output = self._to_numpy(scaled_module(self.standardised_data))
         plain_output = self._to_numpy(base_module(self.standardised_data))
         np.testing.assert_array_less(-np.abs(standard_output - plain_output).mean(), -1e-6)
@@ -64,56 +90,77 @@ class StandardiseModuleTests(unittest.TestCase):
     def _test_standardised_module_gives_different_results_to_plain_module_on_different_data_wrong_way(
         self, base_module: torch.nn.Module, scaled_module: torch.nn.Module
     ) -> None:
+        """
+        Test that a scaled and unscaled module give different results on different input data.
+
+        Here we pass a scaled module an already scaled dataset, and an unscaled module an unscaled dataset.
+        In this case, we would not expect the outputs to match.
+        """
         standard_output = self._to_numpy(scaled_module(self.standardised_data))
         plain_output = self._to_numpy(base_module(self.data))
         np.testing.assert_array_less(-np.abs(standard_output - plain_output).mean(), -1e-6)
 
     def test_mean_and_scale(self) -> None:
+        """Test that the mean and standard deviation of the standardiser match those of the data."""
         torch.testing.assert_close(self.standardiser.mean, self.data.mean(dim=0))
         torch.testing.assert_close(self.standardiser.scale, self.data.std(dim=0))
 
     def test_standardised_mean_gives_same_results_as_plain_mean_on_different_data(self) -> None:
+        """Test mean modules give expected outputs on different data."""
         self._test_standardised_module_gives_same_results_as_plain_module_on_different_data(
             self.base_mean, self.scaled_mean
         )
 
     def test_standardised_mean_gives_different_results_to_plain_mean_on_raw_data(self) -> None:
+        """Test mean modules give expected outputs on unscaled data."""
         self._test_standardised_module_gives_different_results_to_plain_module_on_raw_data(
             self.base_mean, self.scaled_mean
         )
 
     def test_standardised_mean_gives_different_results_to_plain_mean_on_standardised_data(self) -> None:
+        """Test mean modules give expected outputs on scaled data."""
         self._test_standardised_module_gives_different_results_to_plain_module_on_standardised_data(
             self.base_mean, self.scaled_mean
         )
 
     def test_standardised_mean_gives_different_results_to_plain_mean_on_different_data_wrong_way(self) -> None:
+        """Test mean modules give expected outputs on different data."""
         self._test_standardised_module_gives_different_results_to_plain_module_on_different_data_wrong_way(
             self.base_mean, self.scaled_mean
         )
 
     def test_standardised_kernel_gives_same_results_as_plain_kernel_on_different_data(self) -> None:
+        """Test mean modules give expected outputs on different data."""
         self._test_standardised_module_gives_same_results_as_plain_module_on_different_data(
             self.base_kernel, self.scaled_kernel
         )
 
     def test_standardised_kernel_gives_different_results_to_plain_kernel_on_raw_data(self) -> None:
+        """Test mean modules give expected outputs on unscaled data."""
         self._test_standardised_module_gives_different_results_to_plain_module_on_raw_data(
             self.base_kernel, self.scaled_kernel
         )
 
     def test_standardised_kernel_gives_different_results_to_plain_kernel_on_standardised_data(self) -> None:
+        """Test mean modules give expected outputs on scaled data."""
         self._test_standardised_module_gives_different_results_to_plain_module_on_standardised_data(
             self.base_kernel, self.scaled_kernel
         )
 
     def test_standardised_kernel_gives_different_results_to_plain_kernel_on_different_data_wrong_way(self) -> None:
+        """Test mean modules give expected outputs on different data."""
         self._test_standardised_module_gives_different_results_to_plain_module_on_different_data_wrong_way(
             self.base_kernel, self.scaled_kernel
         )
 
     @staticmethod
     def _to_numpy(tensor: torch.Tensor) -> numpy.typing.NDArray[np.floating]:
+        """
+        Convert a torch tensor to a numpy array.
+
+        :param tensor: Torch tensor to convert
+        :return: The provided tensor converted to a numpy array
+        """
         try:
             tensor = tensor.to_dense()
         except AttributeError:
@@ -123,11 +170,16 @@ class StandardiseModuleTests(unittest.TestCase):
 
 class DisableStandardiseModuleTests(StandardiseModuleTests):
     """
-    Just replace the base mean and kernel from the above tests with those
+    Tests for functionality to disable scaling of data.
+
+    Within this, we replace the base mean and kernel from `StandardiseModuleTests` with those
     of a controller on which standard scaling has been disabled.
     """
 
     def setUp(self) -> None:
+        """
+        Setup test by defining a controller object that disables data scaling.
+        """
         super().setUp()
 
         @DisableStandardScaling()
