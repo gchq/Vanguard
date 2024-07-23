538c9e0929011b486b54fc59a3855f6701e6f4d7
rg936672 <162452529+rg936672@users.noreply.github.com>
2024-07-23 13:15:38 +0100

    test: add tests for ApplyLearningRateScheduler

--- tests/units/optimise/test_learning_rate_scheduler.py
+++ tests/units/optimise/test_learning_rate_scheduler.py
@@ -18,23 +18,25 @@ class BasicTests(unittest.TestCase):
     Basic tests for the LearningRateFinder decorator.
     """
 
-    def setUp(self) -> None:
-        """Code to run before each test."""
+    def setUp(self):
+        """Initialise data shared between tests."""
         self.rng = get_default_rng()
         self.dataset = SyntheticDataset(rng=self.rng)
 
+    def test_learning_rate_is_stepped(self) -> None:
+        """Test that the learning rate is modified by the scheduler."""
         num_iters = 33
         step_size = 10
         gamma = 0.9
         initial_lr = 0.2
 
-        self.expected_lr = initial_lr * gamma ** (num_iters // step_size)
+        expected_lr = initial_lr * gamma ** (num_iters // step_size)
 
         @ApplyLearningRateScheduler(torch.optim.lr_scheduler.StepLR, step_size=step_size, gamma=gamma)
         class StepLRAdam(torch.optim.Adam):
             pass
 
-        self.controller = GaussianGPController(
+        controller = GaussianGPController(
             self.dataset.train_x,
             self.dataset.train_y,
             ScaledRBFKernel,
@@ -43,15 +45,36 @@ class BasicTests(unittest.TestCase):
             optim_kwargs={"lr": initial_lr},
             rng=self.rng,
         )
+        controller.fit(num_iters)
 
-        self.train_y_mean = self.dataset.train_y.mean()
-        self.train_y_std = self.dataset.train_y.std()
-
-        self.controller.fit(num_iters)
-
-    def test_learning_rate_is_stepped(self) -> None:
         # TODO: this does look like a _lot_ of protected accesses - review this test?
         # https://github.com/gchq/Vanguard/issues/207
         # pylint: disable=protected-access
-        current_lr = self.controller._smart_optimiser._internal_optimiser._applied_scheduler.get_lr()[0]
-        self.assertAlmostEqual(current_lr, self.expected_lr)
+        current_lr = controller._smart_optimiser._internal_optimiser._applied_scheduler.get_lr()[0]
+        self.assertAlmostEqual(current_lr, expected_lr)
+
+    def test_scheduler_handles_loss_schedules(self):
+        """
+        Test that the decorator handles schedulers that take the loss as a parameter.
+
+        Some schedulers (e.g. StepLR in the above test) take no parameters in their step() method.
+        Some, like, ReduceLROnPlateau, take a required "metrics" parameter.
+        This test checks that the decorator can handle both cases.
+        """
+
+        @ApplyLearningRateScheduler(torch.optim.lr_scheduler.ReduceLROnPlateau)
+        class StepLRAdam(torch.optim.Adam):
+            pass
+
+        controller = GaussianGPController(
+            self.dataset.train_x,
+            self.dataset.train_y,
+            ScaledRBFKernel,
+            self.dataset.train_y_std,
+            optimiser_class=StepLRAdam,
+            optim_kwargs={"lr": 0.2},
+            rng=self.rng,
+        )
+
+        # assertion: this doesn't fail due to some "unexpected argument" or "argument missing" error
+        controller.fit(10)
