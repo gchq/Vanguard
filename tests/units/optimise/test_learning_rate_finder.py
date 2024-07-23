93fab79b44192597602619f5785284d77959d02e
rg936672 <162452529+rg936672@users.noreply.github.com>
2024-07-23 10:41:46 +0100

    test: improve tests for LearningRateFinder

--- tests/units/optimise/test_learning_rate_finder.py
+++ tests/units/optimise/test_learning_rate_finder.py
@@ -3,6 +3,10 @@ Tests for LearningRateFinder.
 """
 
 import unittest
+from unittest.mock import patch
+
+import numpy as np
+from linear_operator.utils.errors import NanError
 
 from tests.cases import get_default_rng
 from vanguard.datasets.synthetic import SyntheticDataset
@@ -13,25 +17,72 @@ from vanguard.vanilla import GaussianGPController
 
 class BasicTests(unittest.TestCase):
     """
-    Basic tests for the LearningRateFinder decorator.
+    Basic tests for the LearningRateFinder class.
     """
 
     @classmethod
     def setUpClass(cls) -> None:
-        """Code to run before all tests."""
+        """Set up data shared between all tests."""
         rng = get_default_rng()
         cls.dataset = SyntheticDataset(rng=rng)
 
-        cls.controller = GaussianGPController(
-            cls.dataset.train_x, cls.dataset.train_y, ScaledRBFKernel, cls.dataset.train_y_std, rng=rng
+    def setUp(self):
+        """Set up the controller for each test."""
+        self.rng = get_default_rng()
+        self.controller = GaussianGPController(
+            self.dataset.train_x, self.dataset.train_y, ScaledRBFKernel, self.dataset.train_y_std, rng=self.rng
         )
 
-        cls.train_y_mean = cls.dataset.train_y.mean()
-        cls.train_y_std = cls.dataset.train_y.std()
+    def test_learning_rate_finder(self) -> None:
+        """Test that the learning rate finder finds the minimum learning rate in the given range."""
+        lr_finder = LearningRateFinder(self.controller)
 
-        cls.lr_finder = LearningRateFinder(cls.controller)
-        cls.lr_finder.find(max_iterations=10, num_divisions=25)
+        min_lr = 1e-5
+        max_lr = 10
+        num_divisions = 25
+        learning_rate_losses = {}
 
-    def test_learning_rate_is_within_bounds(self) -> None:
-        self.assertGreater(self.lr_finder.best_learning_rate, 1e-5)
-        self.assertLess(self.lr_finder.best_learning_rate, 10)
+        # We patch out the _run_learning_rate function as we're just testing the support apparatus here. Instead,
+        # we just generate a random number, and record it for testing.
+        def random_record_output(learning_rate: float, *_):
+            """Generate and record a random number."""
+            loss = self.rng.random()
+            learning_rate_losses[learning_rate] = loss
+            return loss
+
+        with patch.object(lr_finder, "_run_learning_rate", side_effect=random_record_output) as mock_learning_rate:
+            lr_finder.find(num_divisions=num_divisions, start_lr=min_lr, end_lr=max_lr)
+
+        # check we tried the given number of learning rates
+        assert mock_learning_rate.call_count == 25
+
+        # check that the best learning rate is in between the min and max
+        assert min_lr <= lr_finder.best_learning_rate <= max_lr
+
+        # check that our best learning rate is in fact the smallest one we found
+        assert lr_finder.best_learning_rate in learning_rate_losses
+        assert learning_rate_losses[lr_finder.best_learning_rate] == min(learning_rate_losses.values())
+
+    def test_run_learning_rate_infinite_on_fail(self):
+        """Test that if training fails due to NaNs before the max iterations is reached, the loss is infinity."""
+        lr_finder = LearningRateFinder(self.controller)
+
+        with patch.object(self.controller, "fit", side_effect=NanError):
+            # pylint: disable-next=protected-access
+            loss = lr_finder._run_learning_rate(lr=1, max_iterations=10)
+
+        assert loss == np.inf
+
+    def test_run_learning_rate_parameter_pass_through(self):
+        """Test that _run_learning_rate correctly passes through its parameters to the controller."""
+        lr_finder = LearningRateFinder(self.controller)
+
+        learning_rate = 1234
+        max_iterations = 4321
+
+        with patch.object(self.controller, "fit") as mock_fit:
+            # pylint: disable-next=protected-access
+            lr_finder._run_learning_rate(lr=learning_rate, max_iterations=max_iterations)
+
+        mock_fit.assert_called_once_with(n_sgd_iters=max_iterations)
+        assert self.controller.learning_rate == learning_rate
