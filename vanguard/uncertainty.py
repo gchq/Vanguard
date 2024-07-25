f1fae1465e6f61b4b515788ba0dea4c81fd63186
rg936672 <162452529+rg936672@users.noreply.github.com>
2024-07-25 17:18:06 +0100

    fix: remove usage of `x.T` for non-2D tensors (use `x.t()` instead)
    Refs: #279

--- vanguard/uncertainty.py
+++ vanguard/uncertainty.py
@@ -256,7 +256,7 @@ class GaussianUncertaintyGPController(GPController):
         tx = torch.as_tensor(x, dtype=self.dtype, device=self.device)
         tx_std = self._process_x_std(x_std).to(self.device)
         predictions, covar, additive_grad_noise = self._get_additive_grad_noise(tx, tx_std**2)
-        additional_covar = torch.diag(self._noise_transform(additive_grad_noise).T.reshape(-1))
+        additional_covar = torch.diag(self._noise_transform(additive_grad_noise).t().reshape(-1))
         covar += additional_covar
 
         jitter = torch.eye(covar.shape[0]) * gpytorch.settings.cholesky_jitter.value(covar.dtype)
