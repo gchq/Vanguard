12f5a169a36d954e8daa4f971c7f40f4c6e0349f
pc532627 <138115355+pc532627@users.noreply.github.com>
2024-08-19 16:58:10 +0100

    docs: update with develop changes.
    Refs: #240

--- vanguard/datasets/bike.py
+++ vanguard/datasets/bike.py
@@ -1,3 +1,17 @@
+# © Crown Copyright GCHQ
+#
+# Licensed under the GNU General Public License, version 3 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 The bike dataset contains messy information about bike rentals, and is a good dataset for testing performance.
 
@@ -11,7 +25,7 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
-from .basedataset import FileDataset
+from vanguard.datasets.basedataset import FileDataset
 
 
 class BikeDataset(FileDataset):
