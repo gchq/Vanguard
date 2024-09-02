25730a13fa720aee12aa078677738fc90e6c15ff
rg936672 <162452529+rg936672@users.noreply.github.com>
2024-09-02 12:55:09 +0100

    remove: unnecessary library_terms line and noqa hint

--- vanguard/__init__.py
+++ vanguard/__init__.py
@@ -19,7 +19,7 @@ Vanguard contains convenience wrappers for a number of advanced Gaussian Process
 techniques, designed with a focus on simplicity, extension and combination.
 """
 
-from vanguard import _bibliography  # noqa: E402
+from vanguard import _bibliography
 
 __author__ = "GCHQ"
 __version__ = "2.1.0"
