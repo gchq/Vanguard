e04dc2626c28e275c44e653d3e9923fa5b6fb7f4
qh681248 <181246904+qh681248@users.noreply.github.com>
2024-10-04 09:22:55 +0100

    added debug statement

--- vanguard/_bibliography.py
+++ vanguard/_bibliography.py
@@ -21,6 +21,7 @@ import sys
 
 import bibtexparser
 
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 def _import_bibliography(bibtex_file_path, encoding="utf8"):
     """Import a .bib file as a dictionary."""
@@ -36,7 +37,10 @@ def _import_bibliography(bibtex_file_path, encoding="utf8"):
                 try:
                     entry["url"] = "https://arxiv.org/abs/" + entry["eprint"]
                 except KeyError as exc:
-                    raise ValueError(f"Cannot calculate arXiv URL for {reference_id}: missing 'eprint'.") from exc
+                    if DEBUG:
+                        raise ValueError(f"Cannot calculate arXiv URL for {reference_id}: missing 'eprint'.") from exc
+                    else:
+                        raise ValueError("Cannot calculate arXiv URL: missing 'eprint'.") from exc
         else:
             entry["url"] = entry["url"].replace("\n", "")
 
